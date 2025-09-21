b2b1288ab312ce364a52f3bb737ba3acc7d5e589
Chuthuka <chuthuka097dedunu@gmail.com>
2025-09-21 22:17:35 +0530

    add links to sidebar navigation

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -12,9 +12,54 @@ import "./Sidebar.css";
 
 const Sidebar = () => {
 
+ return (
+    <div className="sidebar">
+      <h2 className="logo">MOS Burgers</h2>
+      <ul className="menu">
+        <li>
+          <Link to="/dashboard">
+            <Home size={20} />
+            <span>Dashboard</span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/place-order">
+            <ShoppingCart size={20} />
+            <span>Orders</span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/stock">
+            <ClipboardList size={20} />
+            <span>Inventory</span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/customers">
+            <Users size={20} />
+            <span>Customers</span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/order-history">
+            <FileText size={20} />
+            <span>Order History</span>
+          </Link>
+        </li>
+
+        <li>
+          <li>
+            <Link to="/logout">
+              <LogOut size={20} />
+              <span>Logout</span>
+            </Link>
+          </li>
+        </li>
+      </ul>
+    </div>
+  );
 
 
-  
 };
 
 export default Sidebar;

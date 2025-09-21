b27847c98b8293b92be8d671c7e946f68a25986c
Chuthuka <chuthuka097dedunu@gmail.com>
2025-09-21 22:14:33 +0530

    create Sidebar component

---
+++ src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  Home,
+  ShoppingCart,
+  ClipboardList,
+  Users,
+  LogOut,
+  FileText,
+} from "lucide-react";
+import "./Sidebar.css";
+
+const Sidebar = () => {
+
+
+
+  
+};
+
+export default Sidebar;

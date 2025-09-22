d272abcbcae3f79b57dd04f799b669a60f8740a9
Chuthuka <chuthuka097dedunu@gmail.com>
2025-09-22 07:27:09 +0530

    create Dashboard component

---
+++ src/Components/dashboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+import Sidebar from '../Sidebar/Sidebar';
+import './Dashboard.css';
+import { Users, Box, ShoppingCart } from 'lucide-react';
+import axios from 'axios';
+
+const Dashboard = () => {
+    
+};
+
+export default Dashboard;
+
+

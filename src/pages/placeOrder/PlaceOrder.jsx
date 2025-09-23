b5450a1a38874da42a1eb135d4e05de2b0742fcf
Chuthuka <chuthuka097dedunu@gmail.com>
2025-09-23 07:33:46 +0530

    create PlaceOrder page component

---
+++ src/pages/placeOrder/PlaceOrder.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import "./PlaceOrder.css";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import axios from "axios";
+import jsPDF from "jspdf";
+import "jspdf-autotable";
+
+const PlaceOrder = () => {
+  
+  
+};
+
+export default PlaceOrder;

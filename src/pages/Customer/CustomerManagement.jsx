04643c02f7dc4d6a1c7f957e2dad364590fe4a37
Chuthuka <chuthuka097dedunu@gmail.com>
2025-09-24 07:59:41 +0530

    add base CustomerManagement page structure with Sidebar and layout

---
+++ src/pages/Customer/CustomerManagement.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import "./CustomerManagement.css";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const CustomerManagement = () => {
+  const [customers, setCustomers] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedCustomer, setSelectedCustomer] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [updatedCustomer, setUpdatedCustomer] = useState({
+    id: "",
+    name: "",
+    address: "",
+    contactNumber: "",
+    email: "",
+  });
+
+
+  useEffect(() => {
+    const fetchCustomers = async () => {
+      try {
+        const response = await axios.get("http://localhost:8080/api/customer/all");
+        setCustomers(response.data);  
+      } catch (error) {
+        console.error("Error fetching customer data:", error);
+      }
+    };
+    fetchCustomers();
+  }, []);
+
+  const filteredCustomers = customers.filter((customer) =>
+    customer.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  
+};
+
+export default CustomerManagement;
+
+
+
+
+
+
+
+
